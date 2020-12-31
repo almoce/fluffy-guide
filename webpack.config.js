ac921e06d8a79ba196b4c56cf95fc861dab34bb3
Wence <almo.ce@gmail.com>
2020-12-31 18:37:08 +0800

    :green_heart: add clear webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,101 +4,113 @@ const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-const config = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: !process.env.WEBPACK_DEV_SERVER ? '[name].[contenthash:5].js' : '[name].js',
-        chunkFilename: !process.env.WEBPACK_DEV_SERVER ? '[name].[contenthash:5].js' : '[name].js',
-        publicPath: '',
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+module.exports = (env, arg) => {
+    const { mode } = arg
+    const production = mode === 'production'
+
+    const config = {
+        entry: './src/index.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: !production ? '[name].[contenthash:5].js' : '[name].js',
+            chunkFilename: !production ? '[name].[contenthash:5].js' : '[name].js',
+            publicPath: '',
         },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+            alias: {
+                '@': path.resolve(__dirname, './src'),
             },
-            {
-                test: /\.styl$/,
-                use: [
-                    {
-                        loader: !process.env.WEBPACK_DEV_SERVER ? MiniCssExtractPlugin.loader : 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [['postcss-preset-env']],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(styl|css)$/,
+                    use: [
+                        {
+                            loader: production ? MiniCssExtractPlugin.loader : 'style-loader',
+                        },
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions: {
+                                    plugins: [['postcss-preset-env']],
+                                },
                             },
                         },
-                    },
-                    {
-                        loader: 'stylus-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
+                        {
+                            loader: 'stylus-loader',
                         },
-                    },
-                ],
-            },
+                    ],
+                },
+                {
+                    test: /\.(png|jpg|gif)$/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 8192,
+                                outputPath: production ? 'assets' : '',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        target: 'web',
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                filename: 'index.html',
+                inject: 'body',
+            }),
+            new webpack.DefinePlugin({
+                'process.env.NODD_ENV': JSON.stringify(process.env.NODE_ENV),
+            }),
+            new ESLintPlugin({
+                extensions: ['.tsx', '.ts'],
+            }),
         ],
-    },
-    target: 'web',
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash:5].css',
-            chunkFilename: '[name].[contenthash:5].css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-            inject: 'body',
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODD_ENV': JSON.stringify(process.env.NODE_ENV),
-        }),
-        new ESLintPlugin({
-            extensions: ['.tsx', '.ts'],
-        }),
-    ],
-}
+    }
 
-if (process.env.WEBPACK_DEV_SERVER) {
-    config.devServer = {
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-        // host: '0.0.0.0',
-        disableHostCheck: true,
-        contentBase: path.join(__dirname, 'dist'),
-        proxy: {
-            '/api': {
-                target: 'https://coffeexcx.migu.cn/test',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': '',
+    if (process.env.WEBPACK_DEV_SERVER) {
+        config.devServer = {
+            hot: true,
+            compress: true,
+            historyApiFallback: true,
+            host: '0.0.0.0',
+            disableHostCheck: true,
+            contentBase: path.join(__dirname, 'dist'),
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:8080/test',
+                    changeOrigin: true,
+                    pathRewrite: {
+                        '^/api': '',
+                    },
                 },
             },
-        },
+        }
     }
+    if (production) {
+        const plugins = [
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash:5].css',
+                chunkFilename: '[name].[contenthash:5].css',
+            }),
+            new CleanWebpackPlugin(),
+        ]
+        config.plugins = config.plugins.concat(plugins)
+    }
+    return config
 }
-
-module.exports = config
